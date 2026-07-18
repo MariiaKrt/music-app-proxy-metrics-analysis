4ed76fd172ef816c803d0de9233ccc4c109d2bb5
MariiaKrt <147281321+MariiaKrt@users.noreply.github.com>
2026-07-18 19:46:35 +0200

    Add files via upload

---
+++ src/styles.py
@@ -0,0 +1,16 @@
+
+# ======================
+# style and color helper
+# ======================
+
+colors = {'main': {'light':  ['#EAF6FF', '#FFF0E0', '#DDF7F3', '#FCE4F1', '#EEF1FF', '#F5F7D5'],
+                   'medium': ['#99CEFF', '#FFCA99', '#67C9BF', '#F2A3C9', '#AAB8F8', '#E8E78A'],
+                   'dark':   ['#4C8FBE', '#D98E57', '#1F8D86', '#C85D95', '#6678D9', '#A5A52C']},
+
+          'status': {'positive': '#5EE8C3',
+                     'negative': '#E85E83'},
+
+          'neutral': {'light':  '#F2F4F5',
+                      'medium': '#DADEE3',
+                      'dark':   '#9FAAB7'}
+          }
